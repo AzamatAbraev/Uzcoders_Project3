0119937d2de32694370c560c0096fb173fc14973
AzamatAbraev <azamatabraev07@gmail.com>
2023-07-29 02:24:37 +0500

    setting up structure and codes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./pages/*.html",
+  "./components/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
