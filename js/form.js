ee6ccd3900987d7624a189fd86bfb09c7c5eaa04
AzamatAbraev <azamatabraev07@gmail.com>
2023-08-02 00:26:22 +0500

    profile page added

---
+++ js/form.js
@@ -0,0 +1,6 @@
+function ClearFields() {
+  document.getElementById("full__name").value = " ";
+  document.getElementById("tell__number").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("profile__password").value = "";
+}
